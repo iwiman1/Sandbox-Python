ec846f60fb3cf3136717a4ddd87cea27e44c0161
iwiman1 <ignaciowiman@gmail.com>
2024-05-04 23:08:23 -0300

    Rename of files

---
+++ AdventOfCode2023/Day1/Part1/test_Day1Part1.py
@@ -0,0 +1,12 @@
+import Day1Part1
+import unittest
+
+class Day1Tests(unittest.TestCase):
+    def test_fullSample(self):
+        self.assertEqual(Day1Part1.day1Part1Main("F:\Dev\Sandbox-Python\AdventOfCode2023\Day1\Part1\Inputs\FullSample.txt"), 52974)
+
+    def test_smallSample(self):
+        self.assertEqual(Day1Part1.day1Part1Main("F:\Dev\Sandbox-Python\AdventOfCode2023\Day1\Part1\Inputs\SmallSample.txt"), 142)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ AdventOfCode2023/Day1/Part2/test_Day1Part2.py
@@ -0,0 +1,12 @@
+import Day1Part2
+import unittest
+
+class Day1Tests(unittest.TestCase):
+    def test_fullSample(self):
+        self.assertEqual(Day1Part2.day1Part2Main("F:\Dev\Sandbox-Python\AdventOfCode2023\Day1\Part2\Inputs\FullSample.txt"), 53340)
+
+    def test_smallSample(self):
+        self.assertEqual(Day1Part2.day1Part2Main("F:\Dev\Sandbox-Python\AdventOfCode2023\Day1\Part2\Inputs\SmallSample.txt"), 281)
+
+if __name__ == "__main__":
+    unittest.main()
